69eafa56f6a6360b51ce51bcaee416370f881fee
Hoangminhtien98 <Hmt971998@gmail.com>
2021-12-23 15:11:04 +0700

    chua xong

--- src/Main.java
+++ src/Main.java
@@ -13,30 +13,33 @@ public class Main {
             System.out.println("3. Sửa thông tin Sinh viên bằng id");
             System.out.println("4. Xóa học sinh bằng id");
             System.out.println("5. Tìm sinh viên qua id");
-            System.out.println("6. Range student by average point");
-            System.out.println("7. Tìm sinh viên có điểm trung bình cao nhât");
+            System.out.println("6. Tìm sinh viên có điểm trung bình cao nhât");
             System.out.println("0. Exit");
             System.out.print("Enter you choice: ");
             choice = scanner.nextInt();
             switch (choice) {
-                case 1:
+                case 1: ;
                     managerStudent.addStudent();
+                    break;
                 case 2:
                     managerStudent.displayAllStudent();
                     break;
                 case 3:
-                    managerStudent.editStudentById();
+                    System.out.println("Nhập id sinh viên Muốn sửa: ");
+                    int editId = scanner.nextInt();
+                    System.out.println(managerStudent.editStudentById(editId));
                     break;
                 case 4:
-                    managerStudent.deleteStudentById();
+                    System.out.println("Nhập id sinh viên bạn muốn xóa: ");
+                    int deleteId = scanner.nextInt();
+                    System.out.println(managerStudent.deleteStudentById(deleteId));
                     break;
                 case 5:
-                    managerStudent.findStudentById();
+                    System.out.println("Nhập id sinh viên muốn tìm: ");
+                    int findId = scanner.nextInt();
+                    managerStudent.findStudentById(findId);
                     break;
                 case 6:
-                    managerStudent.
-                    break;
-                case 7:
                     managerStudent.findMaxAveragePoint();
                     break;
             }
